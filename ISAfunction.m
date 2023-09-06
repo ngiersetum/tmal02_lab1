4151733446e36f4132559b6f7f6f9969c3ed67e1
niklas gierse <101125611+ngiersetum@users.noreply.github.com>
2023-09-06 11:17:32 +0200

    cleanup and correction of bet. first plotting stuff

--- ISAfunction.m
+++ ISAfunction.m
@@ -5,44 +5,31 @@ function [T, P, rho, a, mu, liuID1, liuID2] = ISAfunction(Z)
 % Synopsis
 %   [T P rho a mu liuid1 liuid2] = ISAfunction(Z)
 %
-% Description
-%   <Description>
-%
-% Inputs ([]s are optional)
+% Inputs
 %   (scalar or vector) Z      Geometric altitude in [m]
 %
-% Outputs ([]s are optional)
+% Outputs
 %   (scalar or vector) T      Temperature, K
 %   (scalar or vector) P      Pressure, Pa
 %   (scalar or vector) rho    Density, kg/m^3
 %   (scalar or vector) a      Sound speed, m/s
 %   (scalar or vector) mu     Dynamic viscosity, kg/(m s)
 %
-% Examples
-%   <Example Code>
-%
-% See also
-%   <See Also Function Name>
-%
-% Requirements
-%   <Function Name> (<Toolbox Name>)
-
 % References
 %   <Reference List like Papers>
 %   ...
 %
 % Authors
-  liuID1 = "nikgi434"; % author 1 name / <LiU ID 1>
-  liuID2 = "leomu719"; % author 2 name / <LiU ID 2>
+    liuID1 = "nikgi434"; % author 1 name / <LiU ID 1>
+    liuID2 = "leomu719"; % author 2 name / <LiU ID 2>
 %
 % License
 %   This program is part of an academic exercise for the course TMAL02,
 %   Linköping University, year 2023. The program is therefore free for 
 %   non-commercial academic use.
 %
-% Changes
-%   <Date>  <Statement>
-%   ...
+% Code History
+%   https://github.com/ngiersetum/tmal02_lab1
 
 %% The executable code starts here:
 % Define constants and reference values
@@ -55,7 +42,7 @@ function [T, P, rho, a, mu, liuID1, liuID2] = ISAfunction(Z)
     bs = [0, 1, 2, 3, 4, 5, 6, 7];
     S = 110; %[K] (Sutherland Constant)
     Hb =[0, 11000, 20000, 32000, 47000, 51000, 71000, 84852]; %[m']
-    bet = [-6.5, 0, 1, 2.8, 0, -2.8, -2.0];
+    bet = [-0.0065, 0, 0.001, 0.0028, 0, -0.0028, -0.0020]; %[K/m]
     Tb =[288.15, 216.650, 216.650, 228.650, 270.650, 270.560, 214.650, 186.8673]; %[K]
     pb = [101325, 22632, 5474.8, 868.01, 110.9, 66.938, 3.9564, 0.39814]; % [Pa]
     rhob = [1.2250]; %[kg/m³]
@@ -67,22 +54,24 @@ function [T, P, rho, a, mu, liuID1, liuID2] = ISAfunction(Z)
 % Determine number of input altitudes
     nInput = length(Z);
     
-% Convert input (geometrical altitude) to geopotential altitude for
-% calculations
+% Convert input (geometric altitude) to geopotential altitude for
+% calculations. Conversion is done as an individual operation for every
+% altitude in the loop later
     g = @(Z) g0 .* (r0./(r0+Z)).^2;   % function for calculation of g
-%     H = 1/g0 .* integral(g, 0, Z);
-    H = Z;   % initialize array just so it doesn't change size every iteration later
+
+% Initialize arrays just so they don't change size every iteration later
+    H = zeros(size(Z));
+    T = zeros(size(Z));
+    P = zeros(size(Z));
+    rho = zeros(size(Z));
+    a = zeros(size(Z));
+    mu = zeros(size(Z));
         
-% What are the "primary" properties that we need to compute from the
-% reference values and their equations? Which others can be computed later
-% from the primary ones? We start then with the "primary" ones. If instead
-% of a single value, the input contains multiple values, we will need to
-% make a loop so the equations are applied to each one of those inputs.
-% Let's make a loop as long as the number of inputs we caounted before!
-% Type "help for" in the Command Window".
+% Main loop for all calculations
     for j = 1:nInput
 
-        H(j) = 1/g0 .* integral(g, 0, Z(j))   % integral wants scalars so we do it separately for each altitude value
+        % Integral wants scalars so we do it separately for each altitude value
+        H(j) = 1/g0 .* integral(g, 0, Z(j));
 
         b = 1;
         % Now that we are inside the loop, for each one of the inputs, we
@@ -111,11 +100,11 @@ function [T, P, rho, a, mu, liuID1, liuID2] = ISAfunction(Z)
             P(j) = NaN;
         end
 
-        b
+        b;
 
 
-        T(j) = Tb(b) + bet(b) .* (H(j) - Hb(b))
-        P(j) = pb(b) .* (T(j)./(T(j) + bet(j) .* (H(j) - Hb(b)))).^((g0 .* M0)/(R .* bet(b)))
+        T(j) = Tb(b) + bet(b) .* (H(j) - Hb(b));
+        P(j) = pb(b) .* (T(j)./(T(j) + bet(b) .* (H(j) - Hb(b)))).^((g0 .* M0)/(R .* bet(b)));
 
         % Calculate secondary properties: density, speed of sound, and dynamic viscosity
         rho(j) = (P(j).*M0)./(T(j).*R);
@@ -125,16 +114,10 @@ function [T, P, rho, a, mu, liuID1, liuID2] = ISAfunction(Z)
     end
 
 % Output calculated values
-    T
-    P
-    rho
-    a
-    mu
+%     T
+%     P
+%     rho
+%     a
+%     mu
 
 end
-
-% function g = calculate_g(Z)
-%     r0 = 6356766;
-%     g0 = 9.80665;
-%     g = g0 + (r0/(r0+Z))^2;
-% end
--- ISAplot.m
+++ ISAplot.m
@@ -38,20 +38,34 @@ altitudes = 0:50:86000;
     
 % Now it might be time to send those values to our fantastic function, and  
 % get back the properties we need. Tip: type "help function" in the Command  
-% Window.  
-[Ts, Ps, rhos, as, mus, id1, id2] = ISAfunction(50)
+% Window
+[T, p, rho, a, mu, id1, id2] = ISAfunction(altitudes);
 
 % By now we should have all the values we need. However, these are absolute
 % values. If we want to plot all of them in the same axis it might look a
 % bit unreadable. A much better idea would be to express these properties
 % as fractions of their respective sea-level value. Doing this, we make 
 % sure all of them are in the same order of magnitude.
-...
+T0 = 288.15; %[K]
+p0 = 101325; % [Pa]
+rho0 = 1.2250; %[kg/m³]
+a0 = 340.294; %[m/s]
+mu0 = 1.7894e-5; %[kg/ms]
+
+TScaled = T ./ T0;
+pScaled = p ./ p0;
+rhoScaled = rho ./ rho0;
+aScaled = a ./ a0;
+muScaled = mu ./ mu0;
 
 % Now we have what we need! Let's make a figure and plot on it. Hint: type
 % "help plot" in the Command Window.
 figure
-... %plot
+hold on
+grid on
+
+plot(altitudes(:), TScaled(:))
+
 % And, in order to make the figure look decent, we can add and change some
 % of the plot properties (subscripts are like in Latex Math mode indicated
 % by "_{myText}"):
